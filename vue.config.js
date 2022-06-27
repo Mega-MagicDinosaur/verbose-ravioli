bb84b7a03137b0a277f79a506d63d849ad031de0
Alessandro Somigli <alesomirodari@gmail.com>
2022-06-27 23:23:40 +0200

    fixed config error

--- vue.config.js
+++ vue.config.js
@@ -2,13 +2,4 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig(
   {
     transpileDependencies: true,
-
-    // maybe remove:
-    css: {
-      loaderOptions: {
-        scss: {
-          prependData: '@use "sass:math";',
-        },
-      },
-    },
   },)
