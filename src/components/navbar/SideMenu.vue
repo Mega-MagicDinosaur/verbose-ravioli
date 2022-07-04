043715b1ce712c44f013f2317f7f9da4f76e0e90
Alessandro Somigli <alesomirodari@gmail.com>
2022-07-04 10:40:24 +0200

    improved hidden  side menu

--- src/components/navbar/SideMenu.vue
+++ src/components/navbar/SideMenu.vue
@@ -2,73 +2,36 @@
   <div class="side-menu-wrapper">
     <div class="side-menu" :class="this.active ? 'active' : 'inactive'">
       <!-- TITLE -->
-      <div v-if="this.company != null || this.company != undefined" class="menu-title">
-        <GoogleIcon icon="close" class="close-button" @click="clicked" /> <br />
-
-        <div class="top-menu-title">
-          <img :src="this.company.img" class="company-image" />
-          <div class="top-buttons">
-            <div class="top-button-wrapper" v-for="icon in this.icons[0]" :key="icon" >
-              <IconButton class="icon" :icon="icon.icon" />
-              <span class="icon-label">{{ icon.label }}</span>
-            </div>
-          </div>
-        </div>
-
-        <div class="bottom-menu-title">
-          <div class="bottom-buttons">
-            <div
-              class="bottom-button-wrapper" v-for="icon in this.icons[1]" :key="icon" >
-              <IconButton class="icon" :icon="icon.icon" />
-              <span class="icon-label">{{ icon.label }}</span>
-              <!-- add % value eventually -->
-            </div>
-          </div>
-        </div>
-      </div>
+      <SideTitle :company="this.company"/>
 
       <!-- OVERVIEW -->
       <div v-if="this.company != null || this.company != undefined" class="menu-content" >
         <SideOverview :company="this.company"/>
         <SideAddress :company="this.company"/>
       </div>
+      
     </div>
   </div>
 </template>
 
 <script>
-import IconButton from "../generics/IconButton.vue";
-import GoogleIcon from "../generics/GoogleIcon.vue";
-import { side_menu_icons } from "../../assets/js/staticdata.js";
-import store from "../../store/index.js";
-import * as type from "../../store/mutationTypes/types.js";
 
+import SideTitle from "./SideTitle.vue";
 import SideOverview from "./SideOverview.vue";
 import SideAddress from "./SideAddress.vue";
 
 export default {
   name: "SideMenu",
   components: {
-    IconButton,
-    GoogleIcon,
     SideOverview,
     SideAddress,
+    SideTitle
   },
   props: { company: Object, },
-  data() {
-    return { icons: side_menu_icons, };
-  },
   computed: {
     active() { return this.company !== null; },
   },
-  methods: {
-    clicked() {
-      store.dispatch({
-        type: type.SetSelectedCompany,
-        company: null,
-      });
-    },
-  },
+  
 };
 </script>
 
---
+++ src/components/navbar/SideTitle.vue
@@ -0,0 +1,71 @@
+<template>
+  <div
+    v-if="this.company != null || this.company != undefined"
+    class="menu-title"
+  >
+    <GoogleIcon icon="close" class="close-button" @click="clicked" /> <br />
+
+    <div class="top-menu-title">
+      <img :src="this.company.img" class="company-image" />
+      <div class="top-buttons">
+        <div
+          class="top-button-wrapper"
+          v-for="icon in this.icons[0]"
+          :key="icon"
+        >
+          <IconButton class="icon" :icon="icon.icon" />
+          <span class="icon-label">{{ icon.label }}</span>
+        </div>
+      </div>
+    </div>
+
+    <div class="bottom-menu-title">
+      <div class="bottom-buttons">
+        <div
+          class="bottom-button-wrapper"
+          v-for="icon in this.icons[1]"
+          :key="icon"
+        >
+          <IconButton class="icon" :icon="icon.icon" />
+          <span class="icon-label">{{ icon.label }}</span>
+          <!-- add % value eventually -->
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import * as type from "../../store/mutationTypes/types.js";
+import store from "../../store/index.js";
+
+import { side_menu_icons } from "../../assets/js/staticdata.js";
+
+import IconButton from "../generics/IconButton.vue";
+import GoogleIcon from "../generics/GoogleIcon.vue";
+
+export default {
+  name: "SideTitle",
+  components: {
+    IconButton,
+    GoogleIcon,
+  },
+  props: {
+    company: Object
+  },
+  data() {
+    return { icons: side_menu_icons };
+  },
+  methods: {
+    clicked() {
+      store.dispatch({
+        type: type.SetSelectedCompany,
+        company: null,
+      });
+    },
+  },
+};
+</script>
+
+<style scoped lang="scss" src="../../assets/css/navbar/sidetitle_style.scss">
+</style>
