7f8091ee24fe87c2bf7b71bb7b500d5b1b3d3d62
Alessandro Somigli <alesomirodari@gmail.com>
2022-07-04 15:12:03 +0200

    finally added rounded nav buttons

--- src/components/navbar/NavButton.vue
+++ src/components/navbar/NavButton.vue
@@ -3,6 +3,7 @@
         <router-link class="nav-button-link" :to="icon.url">
             <GoogleIcon :icon="icon.icon" class="nav-button-icon"
             :class="(primary_button == 'true')?'primary-button' : ''"/>
+            <div class="horizontal-bar" v-if="primary_button == 'true'"></div>
         </router-link>
     </div>
 </template>
