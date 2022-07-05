425c8bf23a28f736a82a06afb611598c2978f33b
Alessandro Somigli <alesomirodari@gmail.com>
2022-07-05 17:45:03 +0200

    started working on signin/signup

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,14 @@
 const { defineConfig } = require('@vue/cli-service')
+
+const path = require('path')
+
 module.exports = defineConfig(
   {
     transpileDependencies: true,
+
+    publicPath: '../static/frontend/',
+    outputDir: path.resolve(__dirname, '../static/frontend/'),
+    filenameHashing: false,
+    runtimeCompiler: true,
+    devServer: { devMiddleware: { writeToDisk: true, } },
   },)
